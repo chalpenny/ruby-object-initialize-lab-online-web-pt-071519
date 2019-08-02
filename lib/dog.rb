241ff7262aa1883d5ca85d6b37c502ad750d8655
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-02 01:48:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,6 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
-    if @breed = breed
-      return @breed
-    else
-      "Mutt"
-    end
   end
 
   def name=(name)
